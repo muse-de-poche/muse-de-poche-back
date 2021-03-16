b3d00bdc7a1a68722ff9cf6bc31238485fb7126f
Martin <lecerf.mar@orange.fr>
2021-03-16 16:32:24 +0100

    first commit

---
+++ muse-de-poche-back/src/main/java/org/musedepoche/MuseDePocheApplication.java
@@ -0,0 +1,13 @@
+package org.musedepoche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseDePocheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseDePocheApplication.class, args);
+	}
+
+}
